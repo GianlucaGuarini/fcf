260e1f43f0cf57d532208f71b4d2385f5ac99366
Gianluca Guarini <gianluca.guarini@gmail.com>
2020-05-18 23:36:04 +0200

    updated: while flow

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import ifFlow from './if'
 import switchFlow from './switch'
+import whileFlow from './while'
 
 export default {
   if: ifFlow,
-  switch: switchFlow
+  switch: switchFlow,
+  while: whileFlow
 }
